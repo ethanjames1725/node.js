11c1fa531e556e2208fceddf9fc905af9e3f7a36
Ethan Smith <ethanjames1725@gmail.com>
2026-08-18 21:23:01 +0200

    Remove node_modules from tracking

---
+++ chapter_2/index.js
@@ -0,0 +1,28 @@
+const express = require('express') // require express module
+const app = express() // calls express function to start new Express app
+const path = require('path')
+app.use(express.static('public'))
+
+app.listen(3000,() =>{
+    console.log("App listening on port 3000");
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'))
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'about.html'))
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'contact.html'))
+})
+
+app.get('/portfolio',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'portfolio.html'))
+})
+
+app.use((req,res)=>{
+    res.status(404).sendFile(path.resolve(__dirname,'notfound.html'))
+})
---
+++ chapter_4/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+
+const app = new express()
+const ejs = require('ejs')
+app.use(express.static('public'))
+
+app.listen(4000, ()=>{
+    console.log('App listening on port 4000');
+})
+
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+res.render('index');
+})
+app.get('/about',(req,res)=>{
+//res.sendFile(path.resolve(__dirname,'pages/about.html'))
+res.render('about');
+})
+app.get('/contact',(req,res)=>{
+//res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+res.render('contact');
+})
+app.get('/post',(req,res)=>{
+//res.sendFile(path.resolve(__dirname,'pages/post.html'))
+res.render('post')
+})
