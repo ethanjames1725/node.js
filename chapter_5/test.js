3000216965896221bb1bb55552cbc898f02532d5
Ethan Smith <ethanjames1725@gmail.com>
2026-08-20 13:22:11 +0200

    Completed chapter_5

--- chapter_5/test.js
+++ chapter_5/test.js
@@ -1,15 +1,82 @@
 const mongoose = require('mongoose')
 const BlogPost = require('./models/BlogPost')
-
 mongoose.connect('mongodb://localhost/my_database');
 
-BlogPost.create({
-    title: 'Node.js',
-    body: 'Building a Django project using Django and Python'
-})
-.then((BlogPost)=>{
-    console.log(null, BlogPost);
-})
-.catch((error)=>{
-    console.log(error);
-});
+// Find a document using its ID
+// var id = "6a857998e700e5edda4b0143";
+
+// BlogPost.findById(id)
+//     .then(blogpost => {
+//         console.log(blogpost);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+
+
+// Find a document using its ID and update the title
+
+// var id = "6a857c2448e3b30df2f0302f";
+
+// BlogPost.findByIdAndUpdate(id, {
+//     title: 'Django'
+// })
+//     .then(blogpost => {
+//         console.log(blogpost);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+
+
+// Deleting a document.
+
+// var id = "6a857c0102da6d6c3ed3cbef";
+
+// BlogPost.findByIdAndDelete(id)
+//     .then(blogpost => {
+//         console.log(blogpost);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+
+
+// BlogPost.create({
+//     title: 'Node.js',
+//     body: 'Building a Blog project using Express, Node.js, Mongo, Python'
+// })
+//     .then((BlogPost) => {
+//         console.log(null, BlogPost);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+
+// BlogPost.find({})
+//     .then(blogposts => {
+//         console.log(blogposts);
+//     })
+
+
+// BlogPost.find({
+//     title: 'The Mythbuster Guide to Saving Money on Energy Bills'
+// })
+//     .then(blogspot => {
+//         console.log(blogspot)
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+
+// BlogPost.find({
+//     title: '/The/'
+// })
+//     .then(blogspot => {
+//         console.log(blogspot)
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
