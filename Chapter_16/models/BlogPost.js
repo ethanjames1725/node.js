c612477ae62e0e0bdb1fb04cb194dd2da0d5eca6
Ethan Smith <ethanjames1725@gmail.com>
2026-09-02 12:26:39 +0200

    added chapter 16 base

---
+++ Chapter_16/models/BlogPost.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const BlogPostSchema = new Schema({
+    title: String,
+    body: String,
+    userid: { //Page 127
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    datePosted: {
+        type: Date,
+        default: Date.now
+    },
+    image: String
+});
+
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+module.exports = BlogPost
